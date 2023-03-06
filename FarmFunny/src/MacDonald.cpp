43893969c30618892175a59789e8a5aa722d1646
alyvn <82229407+hoangoanh123234@users.noreply.github.com>
2023-03-06 22:29:55 +0700

    fix main

--- FarmFunny/src/MacDonald.cpp
+++ FarmFunny/src/MacDonald.cpp
@@ -12,10 +12,12 @@ MacDonald::~MacDonald()
 void MacDonald::feed(string command)
 {
     //command dạng feed ...
-    cmd = strtok (command," ");
+   /**   string cmd = strtok (command," ");
     if (cmd.compare("animals")) MacDonald::feed_all();
     else
-        if (cmd.compare("chickens")) MacDonald::feed_type();
-        else
+        if (cmd.compare("chickens")) MacDonald::feed_type(1);
+        else{
 
+        }
+    */
 }
